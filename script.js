773e292c60ff072e3fb9629f0c3f84fcff12ee4b
unknown <anasteff@outlook.com>
2021-11-10 14:34:39 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnOpenVideo = document.querySelectorAll('.recipes_video-content')
+const islandsPopup = document.getElementById('popup')
+
+function poPup(){
+    islandsPopup.classList.add('show-popup')
+}
+btnOpenVideo.forEach(b => b.addEventListener('click', poPup))
+
+const btnCloseVideo = document.getElementById('popup-close')
+
+btnCloseVideo.addEventListener('click', ()=> {
+    islandsPopup.classList.remove('show-popup')
+})
